58320f99c5c3701ccc54108d0fcc5646682ef344
igorcoding <kalenichenkoqa@gmail.com>
2020-05-13 22:02:39 -0700

    adding function

--- src/Algos/SortArrayOfZeroOnes.java
+++ src/Algos/SortArrayOfZeroOnes.java
@@ -2,7 +2,16 @@ package Algos;
 
 public class SortArrayOfZeroOnes {
 
+//Given an array of integers.
+    //All elements are either ones or zeros inside the array. Your goal is to order the array in ascending order.
 
+    // [1, 0, 0, 0, 1, 1]
+    // [ 1, 1, 1, 0, 0, 0]
+
+    public void SortArray(int arr[]){
+
+
+    }
 
 
 }
