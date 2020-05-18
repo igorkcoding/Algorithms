6db7928e8f4efb7ea6e598db24517ded53aa9e1d
igorcoding <kalenichenkoqa@gmail.com>
2020-05-17 19:23:25 -0700

    adding junit jars & test

--- src/Algos/SortArrayOfZeroOnes.java
+++ src/Algos/SortArrayOfZeroOnes.java
@@ -8,10 +8,29 @@ public class SortArrayOfZeroOnes {
     // [1, 0, 0, 0, 1, 1]
     // [ 1, 1, 1, 0, 0, 0]
 
-    public void SortArray(int arr[]){
+    public void SortArray(int arr[]) {
+
+        //will know how many ones are in the array
+
+        int count = 0;
+
+        for (int element : arr) {
+
+            count += element;
+        }
+        int endOfZeroIndex = arr.length - count;
 
 
+        for (int i = 0; i < endOfZeroIndex; i++) {
+
+            arr[i] = 0;
+
+        }
+
+        for (int i = endOfZeroIndex; i < arr.length; i++) {
+
+            arr[i] = 1;
+
+        }
     }
-
-
 }
